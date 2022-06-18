f33d00611b02da11db9543b6ac585a1899e69ddf
CarqueMax <mamaxdu44470@gmail.com>
2022-06-18 19:12:29 +0200

    et hop, connexion au vps

--- pnr/src/main/java/pnr/modele/jdbcTahhh.java
+++ pnr/src/main/java/pnr/modele/jdbcTahhh.java
@@ -1,19 +1,18 @@
 package pnr.modele;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class jdbcTahhh {
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://141.94.221.193:3306/bd_pnr","ubuntu","salutLaFamax@2");
-            Statement stmt=con.createStatement();  
-            ResultSet rs=stmt.executeQuery("show databases;");
+            Connection con = DriverManager.getConnection("jdbc:mysql://141.94.221.193:3306/bd_pnr","root","BigoLeBoss@56");
+            Statement stmt = con.createStatement();  
+            ResultSet rs = stmt.executeQuery("show tables;");
             System.out.println("Connected");  
+            while(rs.next()) {
+                System.out.println(rs.getString(1));
+            }
+            con.close(); 
         } catch(Exception e) {
             System.out.println(e);
         }
