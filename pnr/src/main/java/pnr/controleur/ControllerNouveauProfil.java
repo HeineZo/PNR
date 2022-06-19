b7a0f694e78f833854e5d5ce75fed196af2e8e21
Anna <anna.guegan@orange.fr>
2022-06-19 16:00:28 +0200

    NouveauProfilQuiFonctionne

--- pnr/src/main/java/pnr/controleur/ControllerNouveauProfil.java
+++ pnr/src/main/java/pnr/controleur/ControllerNouveauProfil.java
@@ -4,13 +4,16 @@ import java.net.URL;
 import java.sql.*;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class ControllerNouveauProfil extends Controller {
+public class ControllerNouveauProfil extends Controller implements Initializable{
 
 
     @FXML
@@ -20,10 +23,10 @@ public class ControllerNouveauProfil extends Controller {
     private Button btnCreer;
 
     @FXML
-    private TextField txtNom;
+    private TextField txtPrenom;
 
     @FXML
-    private TextField txtPrenom;
+    private TextField txtPseudo;
 
     @FXML
     private TextField txtMdp;
@@ -31,12 +34,25 @@ public class ControllerNouveauProfil extends Controller {
     @FXML
     private ComboBox<String> cbPerm;
 
+    private ObservableList<String> permissionChoices = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.permissionChoices.add("Utilisateur");
+        this.permissionChoices.add("Administrateur");
+        this.cbPerm.setItems(this.permissionChoices);
+    }
+
     @FXML
     private void handleBtnClick(ActionEvent event) {
         if (event.getSource() == btnBack) {
             loadStage("../vue/GererProfilsVide.fxml", event);
-        } else if((event.getSource() == btnCreer)&&(txtMdp.getText() != null)&&(cbPerm.getValue() != null)&&(txtNom.getText() != null)) {
-            connect.executeUpdate("INSERT INTO Utilisateur VALUES(2,"+this.txtPrenom.getText()+","+this.txtMdp.getText()+","+this.getPerm(this.cbPerm));
+        } else if((event.getSource() == btnCreer)&&(txtMdp.getText() != null)&&(cbPerm.getValue() != null)&&(txtPrenom.getText() != null)) { 
+            System.out.println(this.txtPseudo.getText());
+            System.out.println(this.txtMdp.getText());
+            System.out.println(this.getPerm(this.cbPerm));
+            System.out.println(this.txtPrenom.getText());
+            connect.executeUpdate("INSERT INTO Utilisateur VALUES('"+this.txtPseudo.getText()+"','"+this.txtMdp.getText()+"','"+this.getPerm(this.cbPerm)+"','"+this.txtPrenom.getText()+"');");
             System.out.println("Utilisateur créé");
         } else {
             System.out.println("controller profil : Données nulles");
