eddd11d2798b6b4313eee7e828ea297608a9e152
HeineZo <aypierre30@gmail.com>
2022-06-19 17:24:03 +0200

    Confirmation working

--- pnr/src/main/java/pnr/controleur/ControllerConfirmation.java
+++ pnr/src/main/java/pnr/controleur/ControllerConfirmation.java
@@ -1,5 +1,20 @@
 package pnr.controleur;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControllerConfirmation {
-    
+
+    @FXML
+    private Button btnCreer;
+
+    @FXML
+    private Button btnCreer1;
+
+    @FXML
+    void handleBtnClick(ActionEvent event) {
+
+    }
+
 }
