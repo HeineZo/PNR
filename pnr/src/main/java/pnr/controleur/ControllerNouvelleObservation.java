9a1383ade70e494bc84ad0965f4633ec4a94b6c5
HeineZo <aypierre30@gmail.com>
2022-06-19 12:09:53 +0200

    📦 NEW: [NouvelleObservation] Choix observateurs

--- pnr/src/main/java/pnr/controleur/ControllerNouvelleObservation.java
+++ pnr/src/main/java/pnr/controleur/ControllerNouvelleObservation.java
@@ -1,19 +1,47 @@
 package pnr.controleur;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.*;
 import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import io.github.palexdev.materialfx.controls.MFXCheckListView;
+import io.github.palexdev.materialfx.controls.MFXListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 
-public class ControllerNouvelleObservation extends Controller{
+public class ControllerNouvelleObservation extends Controller implements Initializable{
     @FXML
-    private AnchorPane rootPane;
-
-    @FXML
-    private GridPane gridPane;
+    private MFXCheckListView<String> observatorList;
 
     @FXML
     private Button btnBack;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ArrayList<String> list = new ArrayList<String>();
+        
+        ResultSet rs = connect.executeQuery("SELECT * FROM Observateur");
+        try {
+            while (rs.next()){
+                list.add(rs.getString("nom") + " " + rs.getString("prenom"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        ObservableList<String> listObservator = FXCollections.observableArrayList(list);
+        observatorList.setItems(listObservator);
+        observatorList.features().enableBounceEffect();
+		observatorList.features().enableSmoothScrolling(0.5);
+        
+    }
 
     @FXML
     private void handleBtnClick(ActionEvent event) {
@@ -21,5 +49,8 @@ public class ControllerNouvelleObservation extends Controller{
             loadStage("../vue/ChoixAction.fxml", event);
         }
     }
+
+
+
 
 }
