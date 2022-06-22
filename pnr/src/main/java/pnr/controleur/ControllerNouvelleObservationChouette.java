f04af9f03d13b94d61496c0c3c5e9677704955ef
HeineZo <aypierre30@gmail.com>
2022-06-22 18:11:43 +0200

    👌 IMPROVE: Chouette init

--- pnr/src/main/java/pnr/controleur/ControllerNouvelleObservationChouette.java
+++ pnr/src/main/java/pnr/controleur/ControllerNouvelleObservationChouette.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
+import javafx.util.StringConverter;
+import pnr.modele.donneeAddsOn.TabObservateur;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.image.ImageView;
@@ -16,14 +18,20 @@ import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXCheckListView;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXDatePicker;
+import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
 import io.github.palexdev.materialfx.controls.MFXListView;
 import io.github.palexdev.materialfx.controls.MFXScrollPane;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import io.github.palexdev.materialfx.utils.DateTimeUtils;
+import io.github.palexdev.materialfx.utils.StringUtils;
+import io.github.palexdev.materialfx.utils.others.FunctionalStringConverter;
 import io.github.palexdev.materialfx.utils.others.dates.DateStringConverter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -50,8 +58,8 @@ public class ControllerNouvelleObservationChouette extends Controller implements
     private MFXDatePicker datePicker;
 
     @FXML
-    private ComboBox<String> cbObservateur;
-    private ObservableList<String> observateur = FXCollections.observableArrayList();
+    private MFXFilterComboBox<TabObservateur> cbObservateur = new MFXFilterComboBox<>();
+    private ObservableList<TabObservateur> observateur = FXCollections.observableArrayList();
 
     @FXML
     private MFXTextField txtHeure;
@@ -63,33 +71,43 @@ public class ControllerNouvelleObservationChouette extends Controller implements
     private MFXTextField txtCoordX;
 
     @FXML
-    private ComboBox<String> cbTypeObs;
+    private MFXComboBox<String> cbTypeObs;
     private ObservableList<String> typeObs = FXCollections.observableArrayList();
 
     @FXML
-    private ComboBox<String> cbEspece;
+    private MFXComboBox<String> cbEspece;
     private ObservableList<String> espece = FXCollections.observableArrayList();
 
     @FXML
-    private ComboBox<String> cbProto;
+    private MFXComboBox<String> cbProto;
     private ObservableList<String> protocole = FXCollections.observableArrayList();
 
     @FXML
-    private ComboBox<String> cbSexe;
+    private MFXComboBox<String> cbSexe;
     private ObservableList<String> sexe = FXCollections.observableArrayList();
+
+    private String idObs;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        ResultSet rs = connect.executeQuery("SELECT pseudonyme FROM Utilisateur ORDER BY pseudonyme;");
+        if (getUserClicked() != null) {
+            idObs = getUserClicked();
+            modifierObs();
+            resetUserClicked();
+        }
+        ResultSet rs = connect.executeQuery("SELECT * FROM Observateur ORDER BY nom,prenom;");
 
         try {
             while (rs.next()) {
-                this.observateur.add(rs.getString("pseudonyme"));
+                if(rs.getString("nom") != null){
+                   this.observateur.add(new TabObservateur(rs.getInt("idObservateur"), rs.getString("nom"), rs.getString("prenom"))); 
+                }
             }
+            StringConverter<TabObservateur> converter = FunctionalStringConverter.to(person -> (person == null) ? "" : person.getNom() + " " + person.getPrenom());
+            Function<String, Predicate<TabObservateur>> filterFunction = s -> obs -> StringUtils.containsIgnoreCase((CharSequence) converter.toString(obs), (CharSequence) s);
             this.cbObservateur.setItems(this.observateur);
+            this.cbObservateur.setFilterFunction(filterFunction);
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
@@ -113,7 +131,44 @@ public class ControllerNouvelleObservationChouette extends Controller implements
         this.cbTypeObs.setItems(this.typeObs);
     }
 
+    @FXML
+    private void handleBtnClick(ActionEvent event) throws SQLException {
+        if (this.nameEspece.getText().equals("Modifier une observation")) {
+            if (event.getSource() == btnBack) loadStage("../vue/DernierObservation.fxml", event);
+            else if (event.getSource() == envoi) updateDonnees(event);
 
+        } else {
+            if (event.getSource() == btnBack) loadStage("../vue/ChoixAction.fxml", event);
+            else if (event.getSource() == envoi) ajouteDonnees(event);
+        }
+    }
+
+    private void modifierObs() {
+        this.nameEspece.setText("Modifier une observation");
+        this.txtCoordX.setDisable(true);
+        this.txtCoordY.setDisable(true);
+        ResultSet rs = connect.executeQuery("SELECT * FROM Obs_Loutre JOIN Observation ON ObsL=idObs JOIN AObserve ON ObsL=lObservation WHERE ObsL = '" + this.idObs + "';");
+        try {
+            while (rs.next()) {
+                // this.txtDate.setText(rs.getString("dateObs"));
+                // this.cbObservateur.setValue(rs.getString("lobservateur"));
+                // this.txtHeure.setText(rs.getString("heureObs"));
+                // this.txtCoordY.setText(rs.getString("lieu_Lambert_Y"));
+                // this.txtCoordX.setText(rs.getString("lieu_Lambert_X"));
+                // this.txtCommune.setText(rs.getString("commune"));
+                // this.txtLieuDit.setText(rs.getString("lieuDit"));
+                // this.cbIndice.setValue(rs.getString("indice"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void ajouteDonnees(ActionEvent event) throws SQLException{
+    }
+
+    private void updateDonnees(ActionEvent event) throws SQLException{
+    }
 
     // @Override
     // public void initialize(URL location, ResourceBundle resources){
@@ -177,16 +232,5 @@ public class ControllerNouvelleObservationChouette extends Controller implements
     //     scrollPane.setContent(pane);
 
     // }
-
-    @FXML
-    private void handleBtnClick(ActionEvent event) {
-        if (event.getSource() == btnBack) {
-            loadStage("../vue/ChoixAction.fxml", event);
-        } else if (event.getSource() == envoi) {
-            
-            initConfirmation("AjouterObservation");
-            loadStage("../vue/Confirmation.fxml", event);
-        }
-    }
 
 }
