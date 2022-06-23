dc22fc86964fe099f5c9505de7b7e90c8ddd09ac
CarqueMax <mamaxdu44470@gmail.com>
2022-06-23 03:10:09 +0200

    Choix Nids GCI

---
+++ pnr/src/main/java/pnr/controleur/ControllerChoixNidGCI.java
@@ -0,0 +1,45 @@
+package pnr.controleur;
+
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class ControllerChoixNidGCI extends Controller {
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private MFXButton btnNon;
+
+    @FXML
+    private MFXButton btnOui;
+
+    @FXML
+    private ImageView imgEspece;
+
+    @FXML
+    private Text nameEspece;
+
+    @FXML
+    private MFXScrollPane scrollPane;
+
+    @FXML
+    private Text textChoixNid;
+
+    @FXML
+    void handleBtnClick(ActionEvent event) {
+        if (event.getSource() == btnBack) {
+            loadStage("../vue/ChoixAction.fxml", event);
+        } else if (event.getSource() == btnNon) {
+            loadStage("../vue/NouvelleObservationGCI.fxml", event);
+        } else if (event.getSource() == btnOui) {
+            // a changer quand la page sera faite
+            //loadStage("../vue/NouveauProfil.fxml", event);
+        }
+    }
+}
