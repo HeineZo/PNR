a07b8a8c3115a3b2b5fc281bd00c73383772d804
CarqueMax <mamaxdu44470@gmail.com>
2022-06-23 09:02:14 +0200

    toujours la javadoc

--- pnr/src/main/java/pnr/controleur/ControllerCreerUneFicheEspece.java
+++ pnr/src/main/java/pnr/controleur/ControllerCreerUneFicheEspece.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 
+/**
+ * Manages the public CreerUneFicheEspece page
+ */
 public class ControllerCreerUneFicheEspece extends Controller{
     
     @FXML
@@ -12,6 +15,13 @@ public class ControllerCreerUneFicheEspece extends Controller{
     @FXML 
     private Button envoi;
 
+    /**
+     * If the user clicks the "Back" button, load the "ChoixAction" scene. If the user clicks the "No"
+     * button, load the "NouvelleObservationGCIsansNid" scene. If the user clicks the "Yes" button,
+     * load the "NouvelleObservationGCIavecNid" scene
+     * 
+     * @param event the event that triggered the method
+     */
     @FXML
     private void handleBtnClick(ActionEvent event) {
         if (event.getSource() == btnBack) {
